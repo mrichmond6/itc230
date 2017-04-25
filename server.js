bf60674d4ece8526de63155b1a2107cbdf9a89bd
mrichmond6 <drawinggravity@gmail.com>
2017-04-24 21:17:24 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,40 +1,63 @@
-var http = require("http");
-var url = require("url");
-var fs = require("fs");
+'use strict'
 
-function start(route, handle) {
-    function onRequest(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for" + pathname + " received.");
+var http = require("http"), fs = require('fs'), qs = require("querystring");
+var book = require('./public/book.js');
 
-        if (request.url === "/") {
-            fs.readFile("public/home.html", function(err, data) {
-                response.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                response.write(data);
-                response.end();
+function serveStaticFile(res, path, contentType, responseCode){
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err,data){
+        if(err){
+            res.writeHead(500, {
+                'Content-Type': 'text/plain'
             });
-        } else if (request.url === "/about"){
-            fs.readFile("package.json", function(err, data) {
-                response.writeHead(200, {
-                    'Content-Type': 'text/html'
-                });
-                response.write(data);
-                response.end();
+            res.end('500 - Internal Error');
+        }else{
+            res.writeHead(responseCode,{
+                'Content-Type': contentType
             });
-            
-        }else {
-            response.writeHead(200, {
-                'Content-Type': 'text/html'
-            });
-            response.write("404 page not found");
-            response.end();
+            res.end(data);
         }
-    }
-
-    http.createServer(onRequest).listen(3000);
-    console.log("Server has started.");
+    });
 }
 
-exports.start = start;
+http.createServer((req,res) => {
+    let url = req.url.split("?");
+    let params = qs.parse(url[1]);
+    let path = url[0].toLowerCase();
+
+    switch(path){
+        case '/':
+            serveStaticFile(res, '/public/home.html', 'text/html');
+            break;
+        
+        case '/about':
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            break;
+                
+        case '/get':
+                let found = book.get(params.title);
+                res.writeHead(200,{
+                    'Content-Type': 'text/plain'
+                });
+                let results = (found) ? JSON.stringify(found) : "Not found";
+                res.end('Results for ' + params.title + "\n" + results);
+                break;
+                
+        case '/delete':
+                res.writeHead(200,{
+                    'Content-Type': 'text/plain'
+                });
+                res.end('This book has been deleted from our records!');
+                break;
+                
+        default:
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain'
+                });
+                res.end('404:Page not found.');
+        }
+}).listen(process.env.PORT || 3000);
+
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
