d124617e528e8f43ed65713c61defebba7541736
mrichmond6 <drawinggravity@gmail.com>
2017-04-24 21:18:21 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var server=require("./server");
-var router=require("./router");
-var requestHandlers=require("./requestHandlers");
-
-var handle={}
-handle["/"]=requestHandlers.start;
-handle["/about"]=requestHandlers.about;
-
-server.start(router.route, handle);
