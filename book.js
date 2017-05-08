5551b7828fd6285d5acf1310f71d16f7b6c9aa01
mrichmond6 <drawinggravity@gmail.com>
2017-05-07 23:48:58 -0700

    .

---
+++ book.js
@@ -0,0 +1,40 @@
+'use strict'
+
+var books = [
+    { 
+        title: "Harry"
+    },
+    { 
+        title: "Potter"
+    },
+    { 
+        title: "Prisoner"
+    },
+    { 
+        title: "Goblet"
+    }, 
+    { 
+        title: "Pheonix"
+    },
+    { 
+        title: "Prince"
+    },
+    { 
+        title: "Hallows"
+    },
+    ]
+
+exports.get = (title)  => {
+    return books.find((item) => {
+        return item.title == title;
+    });
+};
+
+exports.delete = (title) => {
+    //console.log(title);
+    let newBooks = books.filter((item) => {
+        return item.title !== title;
+    });
+    books = newBooks;
+    return books.length;
+};
